4fee39e401f1591120e7c8d0f4bcc76b03308edb
Bianca <biancadossantossc@gmail.com>
2025-07-02 20:39:29 -0300

    Private Page configações de rota

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import './App.css'
 import React from 'react'
 import Home from './components/homepage/Home'
+import PrivatePage from './components/privatepage/PrivatePage.jsx'
 import LoginForm from './components/loginpage/LoginForm'
 import {Routes, Route, Link, NavLink} from 'react-router-dom'
 
 function App() {
   return (
     <>
-      
     <Routes>
       <Route path="/home" element={<Home />} />
       <Route path="/login" element={<LoginForm />} />
+      <Route path="/pagina-privada" element={<PrivatePage />} />
     </Routes>
 
 
---
+++ src/components/privatepage/PrivatePage.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function Home() {
+  const [projetos, setProjetos] = useState([]);
+  const [busca, setBusca] = useState("");
+  const [projetoSelecionado, setProjetoSelecionado] = useState(null);
+  const [mostrarDialogProjeto, setMostrarDialogProjeto] = useState(false);
+  const [mostrarDialogApoiador, setMostrarDialogApoiador] = useState(false);
+  const [mostrarDialogMensagem, setMostrarDialogMensagem] = useState(false);
+
+  useEffect(() => {
+    carregarProjetos();
+  }, []);
+
+  const carregarProjetos = () => {
+    axios.get("http://localhost:8080/projeto/listar")
+      .then(response => setProjetos(response.data))
+      .catch(error => console.error("Erro ao carregar projetos:", error));
+  };
+
+  const buscarProjeto = () => {
+    axios.get("http://localhost:8080/projeto/buscar", {
+      params: { tituloProjeto: busca }
+    })
+    .then(response => setProjetos(response.data))
+    .catch(error => {
+      if (error.response?.status === 404) {
+        setProjetos([]);
+        setMostrarDialogMensagem(true);
+      }
+    });
+  };
+
+  const visualizarProjeto = (id) => {
+    axios.get(`http://localhost:8080/projeto/visualizar/${id}`)
+      .then(response => {
+        setProjetoSelecionado(response.data);
+        setMostrarDialogProjeto(true);
+      })
+      .catch(error => console.error("Erro ao visualizar projeto:", error));
+  };
+
+  const apoiarProjeto = () => {
+    setMostrarDialogProjeto(false);
+    setMostrarDialogApoiador(true);
+  };
+
+  const enviarApoio = () => {
+    setMostrarDialogApoiador(false);
+    setMostrarDialogMensagem(true);
+  };
+
+  return (
+
+    <div className="home-wrapper">
+      <Header
+        busca={busca}
+        setBusca={setBusca}
+        buscarProjeto={buscarProjeto}
+      />
+      <ProjetoTable
+        projetos={projetos}
+        onVisualizar={visualizarProjeto}
+      />
+      {mostrarDialogProjeto && (
+        <DialogProjeto
+          projeto={projetoSelecionado}
+          onFechar={() => setMostrarDialogProjeto(false)}
+          onApoiar={apoiarProjeto}
+        />
+      )}
+      {mostrarDialogApoiador && (
+        <DialogApoiador
+          onFechar={() => setMostrarDialogApoiador(false)}
+          onEnviar={enviarApoio}
+          idProjeto={projetoSelecionado?.id}
+        />
+      )}
+      {mostrarDialogMensagem && (
+        <DialogMensagem
+          onFechar={() => setMostrarDialogMensagem(false)}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Home;
